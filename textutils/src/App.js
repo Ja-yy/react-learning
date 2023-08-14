bb544276421a2ac015de9a557225669433300929
ja-yy <jaypatel.learn@gmail.com>
2023-08-14 16:07:40 +0530

    Added:TextUtils App

---
+++ textutils/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./App.css";
+// import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+
+function App() {
+  const [mode, setMode] = useState("light");
+  const toggelMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
+  return (
+    <>
+      <Navbar title="TextUtils" mode={mode} toggelMode={toggelMode} />
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze" mode={mode} />
+        {/* <About /> */}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ textutils/src/components/TextForm.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const [btntext, setBtnText] = useState("");
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleVlClick = () => {
+    let newText = text.match(/[aeiou]/gi).length;
+    setBtnText(`Total number of vowels: ${newText}`);
+  };
+
+  const HandleExtraSpace = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+  const handleOnChnaged = (event) => {
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div
+        className="container"
+        style={{
+          color: props.mode === "dark" ? "white" : "#042743",
+        }}
+      >
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            id="myBox"
+            rows="8"
+            onChange={handleOnChnaged}
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "#042743",
+            }}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>
+          Convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleVlClick}>
+          Count vowels
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleCopy}>
+          Create copy
+        </button>
+        <button className="btn btn-primary mx-2" onClick={HandleExtraSpace}>
+          Remove extra space
+        </button>
+      </div>
+      <div
+        className="container my-3"
+        style={{
+          color: props.mode === "dark" ? "white" : "#042743",
+        }}
+      >
+        <h2>your text summary</h2>
+        <p>
+          {text.split(" ").length} words {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <p>{btntext}</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Enter something to preview it"}</p>
+      </div>
+    </>
+  );
+}
